3ce05aca211fefe9aa7f55961261d0c9654487d6
Thiago Leite <thiagofmleite@gmail.com>
2025-01-08 10:39:42 -0300

    Add Sidebar component and update layout with CSS grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 import { Post } from "./Post";
 
 import "./styles/global.css";
+import styles from "./App.module.css";
 
 export function App() {
   return (
     <>
       <Header />
-      <div>
-        <Post
-          title="Title 1"
-          author="Diego Fernandes"
-          content="Content content content"
-        />
-        <Post
-          title="Title 2"
-          author="Diego Fernandes"
-          content="Content content content"
-        />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            title="Title 1"
+            author="Diego Fernandes"
+            content="Content content content"
+          />
+          <Post
+            title="Title 2"
+            author="Diego Fernandes"
+            content="Content content content"
+          />
+        </main>
       </div>
     </>
   );
