ac61edd8d47d1f6f9b0da5203c7a7451ad247926
Thiago Leite <thiagofmleite@gmail.com>
2025-01-08 10:39:37 -0300

    Add Sidebar component with styles and profile layout

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,31 @@
+import { PencilLine } from "@phosphor-icons/react";
+
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="/developer-cover.jpg"
+      />
+
+      <div className={styles.profile}>
+        <img
+          src="https://avatars.githubusercontent.com/u/9437391?v=4"
+          alt="User Name"
+          className={styles.avatar}
+        />
+        <strong>User Name</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
