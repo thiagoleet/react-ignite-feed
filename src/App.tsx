c8c655f10d952a2c19bce205c0e48243f9ca6671
Thiago Leite <thiagofmleite@gmail.com>
2025-01-10 13:58:13 -0300

    Rename App entry file from JSX to TSX and reorder import statements

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { Header } from "./components/Header";
-import { Sidebar } from "./components/Sidebar";
-import { Post } from "./components/Post";
-
 import "./styles/global.css";
 import styles from "./App.module.css";
+import { Post } from "./components/Post";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
 const posts = [
   {
